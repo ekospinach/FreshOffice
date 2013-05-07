6defe15c57c7d38ceedc4b9e993a64e2cd97f4ec
Roman <romav@bk.ru>
2013-05-07 18:47:08 +0400

    ewf

---
+++ FreshOffice Desktop/FreshOffice Desktop/AppDelegate.h
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.h
+//  FreshOffice Desktop
+//
+//  Created by iDeveloper on 07.05.13.
+//  Copyright (c) 2013 iDeveloper. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+
+- (IBAction)saveAction:(id)sender;
+
+@end
---
+++ FreshOffice Desktop/FreshOffice Desktop/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  FreshOffice Desktop
+//
+//  Created by iDeveloper on 07.05.13.
+//  Copyright (c) 2013 iDeveloper. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
